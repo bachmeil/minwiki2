358702549c23223ae28a331669167d9e6087ce99
Lance Bachmeier <lanceb@ksu.edu>
2021-06-07 21:09:32 -0500

    Removed dependency on web.d and template.d

--- wiki.d
+++ wiki.d
@@ -1,5 +1,5 @@
-import arsd.cgi, arsd.web;
-import minwiki.replacelinks, minwiki.tem;
+import arsd.cgi;
+import minwiki.replacelinks;
 import dmarkdown.markdown;
 import std.file, std.path, std.process, std.stdio;
 
@@ -16,46 +16,13 @@ enum _editor = "geany -i";
 // Change markdown parsing options here
 MarkdownFlags	_mdflags = MarkdownFlags.backtickCodeBlocks|MarkdownFlags.disableUnderscoreEmphasis;
 
-/*
- * Converts text into a web page. Functions should return an Element.
- */
-Element toHtml(string s) {
-	writeln("inside toHtml...");
-	auto d = new Document();
-	writeln("create fragment...");
-	Element f = d.createFragment();
-	f.innerHTML = s;
-	writeln("done making html...");
-	return f;
-}
-
-/*
- * Load a page in the browser.
- * Converts links to the proper html form.
- * Supports Pandoc-style line breaks.
- * 
- * You can set markdown flags (as found in markdown.d) in here.
- */
-Element renderPage(string name) {
-	writeln("rendering...");
-	string f = readText(name ~ ".md").strip;
-	string mdfile = changeLinks(`<a href="editpage?name=` ~ name ~ `">[Edit This Page]</a> <a href="backlinks?pagename=` ~ name ~ `">[Backlinks]</a><br><br>` ~ "\n\n" ~ f);
-	writeln("off to toHtml...");
-	writeln(mdfile.filterMarkdown(_mdflags).replace("<br />", "<br>").toHtml());
-	return mdfile.filterMarkdown(_mdflags).replace("<br />", "<br>").toHtml();
-}
-
 string mdToHtml(string s, string name) {
 	string mdfile = changeLinks(`<a href="editpage?name=` ~ name ~ `">[Edit]</a> <a href="backlinks?pagename=` ~ name ~ `">[Backlinks]</a><br><br>` ~ "\n\n" ~ s);
 	return plaincss ~ mdfile.filterMarkdown(_mdflags);
 }
 
-/*
- * Not used, but included in case you want to run a shell
- * command and return the output as a page.
- */
-Element shellOutput(string cmd) {
-	return toHtml(executeShell(cmd).output);
+string plainHtml(string s) {
+	return plaincss ~ s.filterMarkdown(_mdflags);
 }
 
 void hello(Cgi cgi) {
@@ -78,52 +45,20 @@ void hello(Cgi cgi) {
 		if (!exists(setExtension(name, "md"))) {
 			executeShell(_editor ~ " " ~ setExtension(name, "md"));
 		}
-		data = mdToHtml(readText(setExtension(name, "md")), name);
+		data = mdToHtml(readText(setExtension(name, "md")) ~ `<br><br><a href="/">[&#171; Index]</a>`, name);
+	}
+	else if (cgi.pathInfo == "/tag") {
+		string tagname = cgi.get["tagname"];
+		data = plainHtml("<h1>Tag: " ~ tagname ~ "</h1>\n" ~ fileLinks(executeShell(`grep -Rl '#` ~ tagname ~ `'`).output));
+	}
+	else if (cgi.pathInfo == "/backlinks") {
+		string pagename = cgi.get["pagename"];
+		string cmd = `grep -Rl '\[#` ~ pagename ~ `\]'`;
+		data = plainHtml(`<h1>Backlinks: <a href="viewpage?name=` ~ pagename ~ `">` ~ pagename ~ "</a></h1>\n" ~ fileLinks(executeShell(cmd).output) ~ "<br><hr>" ~ mdToHtml(readText(pagename ~ ".md"), pagename) ~ "<hr><br><br>");
 	}
 	cgi.write(data, true);
 }
 mixin GenericMain!hello;
-
-
-//class Test: ApiProvider {
-/*class Test: Wikisite {
-	export Element viewpage(string name) {
-		writeln("viewpage...");
-		// Note: This does a check for the existence of the directory
-		// Only creates it if it doesn't exist
-		mkdirRecurse(std.path.dirName(name));			
-		if (!exists(name ~ ".md")) {
-			executeShell(_editor ~ " " ~ name ~ ".md");
-		}
-		writeln("going to render...");
-		return renderPage(name);
-	}
-	
-	export Element editpage(string name) {
-		executeShell(_editor ~ " " ~ name ~ ".md");
-		cgi.setResponseLocation("viewpage?name=" ~ name);
-		return renderPage(name);
-	}
-
-	export Element tag(string tagname) {
-		return toHtml("<h1>Tag: " ~ tagname ~ "</h1>\n" ~ fileLinks(executeShell(`grep -Rl '#` ~ tagname ~ `'`).output));
-	}
-
-	export Element backlinks(string pagename) {
-		string cmd = `grep -Rl '\[#` ~ pagename ~ `\]'`;
-		writeln(cmd);
-		return toHtml(`<h1>Backlinks: <a href="viewpage?name=` ~ pagename ~ `">` ~ pagename ~ "</a></h1>\n" ~ fileLinks(executeShell(cmd).output) ~ "<br><hr>" ~ mdToHtml(readText(pagename ~ ".md"), pagename) ~ "<hr><br><br>");
-	}
-	
-	export Element index() {
-		writeln("in the index...");
-		if (!exists("index.md")) {
-			std.file.write("index.md", "# Index Page\n\nThis is the starting point for your wiki. Click the link above to edit.");
-		}
-		return viewpage("index");
-	}
-}
-mixin FancyMain!Test;*/
 
 string fileLinks(string output) {
 	if (output.length == 0) {
@@ -139,3 +74,21 @@ string fileLinks(string output) {
 		return result;
 	}
 }
+
+enum plaincss = `<style>
+body { margin: auto; max-width: 800px; font-size: 120%; margin-top: 20px; }
+a { text-decoration: none; }
+pre > code {
+  padding: .2rem .5rem;
+  margin: 0 .2rem;
+  font-size: 90%;
+  white-space: nowrap;
+  background: #F1F1F1;
+  border: 1px solid #E1E1E1;
+  border-radius: 4px; 
+  display: block;
+  padding: 1rem 1.5rem;
+  white-space: pre; 
+}
+h1, h2, h3 { font-family: sans; font-size: 140%; }
+</style>`;
